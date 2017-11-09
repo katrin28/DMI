8c789e2c1a0ba27a6f175b556fa69eee38d70852
ansiiis1@gmail.com <katrin983@inbox.lv>
2017-11-09 17:24:30 +0200

    20171109_17_45

---
+++ PYTHON/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+print "Zametalsa pozar goluboj!"
+print "Zametalsa \vpozar \vgoluboj"
+print "Pozabilis \vradimie \vdali"
+print "V \vpervij \vraz \vja \vzapel \vpro \vlubovj "
+print "V \vpervij \vraz \votrekajus \vskandalit." 
+  
+
---
+++ PYTHON/variables.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#http://www.cplusplus.com/reference/cstdio/printf/?kw=printf
+
+a = 65
+print type(a)
+print "Mainiga a vertiba ka dec skaitlis: %d"%(a)
+print "Mainiga a vertiba ka hex skaitlis: %x"%(a)
+print "Mainiga a vertiba ka oct skaitlis: %o"%(a)
+print "Mainiga a vertiba ka simbols: %c"%(a)
+
+a = 'A'
+print type(a)
+print "Mainiga a vertiba ka dec skaitlis: %d"%(ord(a))
+print "Mainiga a vertiba ka hex skaitlis: %x"%(ord(a))
+print "Mainiga a vertiba ka oct skaitlis: %o"%(ord(a))
+print "Mainiga a vertiba ka simbols: %c"%(a)
